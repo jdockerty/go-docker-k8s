651a0bc2e25f3cf22242b1a7a75455b85fc47e62
Jack Dockerty <jdockerty19@gmail.com>
2020-01-07 22:41:15 +0000

    Setup of using mux as router.

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/mux"
+	//"html/template"
+	"path"
+	"log"
 )
 
 func serveWebpage() {
-	http.Handle("/tasks", http.StripPrefix("/tasks.html", http.FileServer(http.Dir("C:\\Users\\Jack\\Desktop\\Go\\src\\taskschedulerdashboard\\html\\tasks.html"))))
-	http.ListenAndServe(":3000", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks", serveHome)
 
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	filepath := path.Dir(`C:\Users\Jack\Desktop\Go\src\taskschedulerdashboard\html\tasks.html`)
+    w.Header().Set("Content-type", "text/html")
+    http.ServeFile(w, r, filepath)
 }
 
 func main() {
